f047b7d98397d3ea9396dc38a19f2d5e78fd3170
kaiyin <kaiyin@gaotengasset.com>
2021-08-13 11:01:59 +0800

    fix(test): 加个返回类型

--- test/parse.ts
+++ test/parse.ts
@@ -2,6 +2,6 @@ const knex = require('knex')({ client: 'mysql' });
 
 console.log(knex.select('title', 'author', 'year').from('books').toString())
 
-console.log(knex.avg('sum_column1').from(function () {
+console.log(knex.avg('sum_column1').from(function (): void {
 	this.sum('column1 as sum_column1').from('t1').groupBy('column1').as('t1')
 }).as('ignored_alias').toString())
